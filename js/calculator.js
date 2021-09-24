83d1038e8d6742f63c62fe9dd7826827e3ad9f20
Tatti <tatu@paski.fi>
2021-09-24 18:29:23 +0300

    Got the calculator to work. Now off to validating the inputs.

--- js/calculator.js
+++ js/calculator.js
@@ -1,6 +1,5 @@
 let counter = 0;
 
-
 /*window.onload = function(){
     document.getElementById("firstTextInput").value = 0;
     document.getElementById("secondTextInput").value = 0;
@@ -8,18 +7,44 @@ let counter = 0;
     document.getElementById("counter").innerHTML = "Laskukertoja "+counterCalc+" kpl";
 }*/
 
-$(function() {
+$(function () {
+  console.log("Document Loaded Properly");
+
   $("#firstTextInput").val(0);
   $("#secondTextInput").val(0);
   $("#result").val(0);
   $("#counterCol").text("Laskukertoja " + counter + " kpl.");
+  console.log("counterCol: " + counter);
 });
-$("#calculateBtn").on("click", function() {
+
+function calculate() {
   counter++;
   $("#counterCol").text("Laskukertoja " + counter + " kpl.");
 
-  let selectedText = $("#operationSelect option:selected").text(); // +
-  let selectedValue = $("#operationSelect").val(); 
-  console.log("selectedText: "+selectedText);
-  console.log("selectedValue: "+selectedValue);
-});
+  let selectedText = $("#operationSelect option:selected").text();
+  let selectedValue = $("#operationSelect").val();
+  let firstInput = $("#firstTextInput").val();
+  let firstInputValue = parseInt(firstInput);
+  let secondInput = $("#secondTextInput").val();
+  let secondInputValue = parseInt(secondInput);
+
+  console.log("selectedText: " + selectedText);
+  console.log("selectedValue: " + selectedValue);
+  console.log("selectedFirstTextInput: " + firstInput);
+  console.log("selectedSecondTextInput: " + secondInput);
+
+  //#region If-Else
+  if (selectedValue == 1) {
+    result = firstInputValue + secondInputValue;
+  } else if (selectedValue == 2) {
+    result = firstInputValue - secondInputValue;
+  } else if (selectedValue == 3) {
+    result = firstInputValue * secondInputValue;
+  } else {
+    result = firstInputValue / secondInputValue;
+  }
+  //#endregion
+
+  console.log("Result is: " + result);
+  $("#result").val(result);
+}
