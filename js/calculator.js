9386109eb09e83dcc8954222834cf7d8651e8e8c
Tatti <tatu@paski.fi>
2021-09-24 17:13:18 +0300

    Might just say that I have messed up!

--- js/calculator.js
+++ js/calculator.js
@@ -1,5 +1,6 @@
 let counter = 0;
 
+
 /*window.onload = function(){
     document.getElementById("firstTextInput").value = 0;
     document.getElementById("secondTextInput").value = 0;
@@ -7,15 +8,18 @@ let counter = 0;
     document.getElementById("counter").innerHTML = "Laskukertoja "+counterCalc+" kpl";
 }*/
 
-$(function () {
+$(function() {
   $("#firstTextInput").val(0);
   $("#secondTextInput").val(0);
   $("#result").val(0);
   $("#counterCol").text("Laskukertoja " + counter + " kpl.");
 });
-$("#calculateBtn").click(function () {
-  counter = counter + 1;
+$("#calculateBtn").on("click", function() {
+  counter++;
   $("#counterCol").text("Laskukertoja " + counter + " kpl.");
 
-  
+  let selectedText = $("#operationSelect option:selected").text(); // +
+  let selectedValue = $("#operationSelect").val(); 
+  console.log("selectedText: "+selectedText);
+  console.log("selectedValue: "+selectedValue);
 });
