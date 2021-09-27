eff28164a6b7280c670704e72a626f4f752608a9
Tatti <tatu@paski.fi>
2021-09-27 03:47:57 +0300

    Started MathRandom

--- js/calculator.js
+++ js/calculator.js
@@ -1,6 +1,8 @@
 let counter = 0;
 var firstInputText;
 var secondInputText;
+var min;
+var max;
 
 /*window.onload = function(){
     document.getElementById("firstTextInput").value = 0;
@@ -8,12 +10,17 @@ var secondInputText;
     document.getElementById("result").value = 0;
     document.getElementById("counter").innerHTML = "Laskukertoja "+counterCalc+" kpl";
 }*/
+$(function mathRancomFunc() {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+});
 
 $(function () {
   console.log("Document Loaded Properly");
   //Make MATHRandom to this function to randomise numbers between 0-9
-  $("#firstTextInput").val(0);
-  $("#secondTextInput").val(0);
+  $("#firstTextInput").val(min);
+  $("#secondTextInput").val(max);
   $("#result").val(0);
   $("#counterCol").text("Laskukertoja " + counter + " kpl.");
   console.log("counterCol: " + counter);
@@ -28,10 +35,13 @@ function calculate() {
   firstInputText = $("#firstTextInput").val();
   secondInputText = $("#secondTextInput").val();
 
-
-
   //Here comes the checkInputs
-  if (firstInputText && !isNaN(firstInputText) && secondInputText && !isNaN(secondInputText)) {
+  if (
+    firstInputText &&
+    !isNaN(firstInputText) &&
+    secondInputText &&
+    !isNaN(secondInputText)
+  ) {
     firstInput = parseInt(firstInputText);
     secondInput = parseInt(secondInputText);
     console.log("selectedText: " + selectedText);
@@ -49,28 +59,10 @@ function calculate() {
       result = firstInput / secondInput;
     }
     //#endregion
-
   } else {
     alert("Not a number");
-
   }
 
   console.log("Result is: " + result);
   $("#result").val(result);
-}
-
-function checkInputs() {
-  console.log("firstInputTravel: " + firstInput);
-  console.log("secondInputTravel: " + secondInput);
-
-  //if(!/^[0-9]+$/.test(firstInput) && !/^[0-9]+$/.test(secondInput)){
-  //  alert("Please only enter numeric characters only for your Age! (Allowed input:0-9)")
-  //}
-  if (secondInput == NaN || firstInput == NaN) {
-    console.log("Error");
-  } else if (secondInput == NaN && firstInput == NaN) {
-    console.log("Error");
-  } else {
-    calculate();
-  }
 }
