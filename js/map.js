2a592060548154a2cd9f071e1450ae9d93930ae0
Tatti <tatu@paski.fi>
2021-09-29 20:19:02 +0300

    Map works! Rewrote the JS file.

--- js/map.js
+++ js/map.js
@@ -1,8 +1,19 @@
-console.log('started')
+var streetName = $("#street");
+var cityName = $("#city");
+var map = $("#map");
+var inputStreet;
+var inputCity;
 
+$(function (){
+
+    console.log("Loaded");
+    console.log("streeName: "+streetName+"- cityName: "+ cityName);
+    console.log("Map link: " +map);
+});
 function search(){
-    var street = document.getElementById(street).nodeValue.replace(" ", "");
-    var city = document.getElementById(city).nodeValue.replace(" ", "");
+    inputStreet = $("#street").val();
+    inputCity = $("#city").val();
+    console.log("streetName: "+inputStreet+" - cityName: "+ inputCity);
 
-    document.getElementById(map).setAttribute("src", "https://www.google.com/maps?q=" + street + city + "&output=embed");
+    $("#map").attr("src", "https://www.google.com/maps?q="+inputStreet+inputCity+"&output=embed");
 }
